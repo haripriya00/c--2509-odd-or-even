281a29f64a12f9d23e9002f85ab80aa8578ae1d1
haripriya00 <109473332+haripriya00@users.noreply.github.com>
2024-10-07 09:21:26 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eg2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+
+class Program
+        {
+           
+
+                Console.WriteLine("Enter a number:");
+                int number = Convert.ToInt32(Console.ReadLine());
+                switch (number % 2)
+                {
+                    case 0:
+                        Console.WriteLine($"{number} is even.");
+                        break;
+
+                    case 1:
+                        Console.WriteLine($"{number} is odd.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid input.");
+                        break;
+                }
+
+            }
+
+        }
+
+    }
+
+}
+
+
